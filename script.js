f862f9273283f2d5e5bee356681cc1e0ce8214f5
skmara2 <skmara2@inbox.lv>
2025-04-15 18:17:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let summa=0;
-let stundas=[0,0,0,0,0,0,0,0,0,0,0];
+let stundas=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 let punkti=0;
 let temas=0;
 let ii;
 let ri;
 let g=0;
+let f;
 function viens(){
   g=0;
   temas=document.getElementById("in1").value;
@@ -21,7 +22,13 @@ function divi(){
   if(p.length>0){
   punkti=Number(p)
   document.getElementById("grupa2").style.display="none";
-  let f="tema3.html"
+    if (temas>10){
+      f="tema5.html"
+    }
+    else{
+      f="tema3.html"
+    }
+  
   fetch(f)
   .then(atbilde => atbilde.text())
   .then(html => {
@@ -38,35 +45,52 @@ function divi(){
 
 
 function kopa(){
-  
+  document.getElementById("in1").value="";
+  document.getElementById("in2").value="";
   if(g==0){
-  for (let i = 1; i <= temas; i++) {
-    ii="t"+i
-    let s=document.getElementById(ii).value;
-    if (s.length>0){
-    console.log(ii)
-    summa+=Number(s);
-    stundas[i]=Number(s)
-    console.log(stundas)
-    console.log(summa)
+    for (let i = 1; i <= temas; i++) {
+      ii="t"+i
+      let s=document.getElementById(ii).value;
+        if (s.length>0){
+          summa+=Number(s);
+          stundas[i]=Number(s)
+        }
+        else{
+          summa=0;
+          stundas=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+          punkti=0;
+          temas=0;
+          alert("Nav visi dati!\nSāc no jauna ar tēmu un punktu skaitu!");
+        }
     }
-    else{
-      alert("Nav visi dati!");
-    }
+    
   }
-  let v=punkti/summa
-  console.log(v)
-  for (let i = 1; i <= temas; i++) {
-    let r=Math.round(stundas[i]*v);
-    console.log(r)
-    ri="rez"+i
-    document.getElementById(ri).value=r;
-  }
-  }
-  else{
-    alert("Punkti jau ir aprēķināti!");
+  else{ 
+    summa=0;
+    stundas=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    punkti=0;
+    temas=0;
+    alert("Punkti jau ir aprēķināti!\nSāc atkal ar tēmu un punktu skaitu!");
   }
   g+=1;
+  if(summa>0){
+    kopa1();
   }
+}
+  
+function kopa1(){  
+  let v=punkti/summa
+  for (let i = 1; i <= temas; i++) {
+    let r=Math.round(stundas[i]*v);
+    ri="rez"+i
+    document.getElementById(ri).value=r;
+    }
+  summa=0;
+  stundas=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  punkti=0;
+  temas=0;
+}
+  
+
   
  
